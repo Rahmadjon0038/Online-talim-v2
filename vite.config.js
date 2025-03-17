0a6458cd081167d00fec0cbc287e71ee0012c329
Murodillayev Hojiakbar <102051063+uzhojiakbar@users.noreply.github.com>
2025-03-17 08:24:51 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,11 +7,19 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://37.27.215.130:5013", // Proksi sifatida ishlatiladigan server manzili
+        // target: "http://37.27.215.130:5013", // Proksi sifatida ishlatiladigan server manzili
+        target: "http://37.27.215.130:5013/", // Backend manzili
+
         changeOrigin: true, // Serverga kelayotgan so'rovni o'zgartirish
         secure: false, // Agar HTTPS bo'lsa, buni true qilib qo'yishingiz mumkin
         // pathRewrite: { '^/api': '' }, // Agar yo'lni o'zgartirmoqchi bo'lsangiz
       },
     },
+  },
+  server: {
+    port: 5014,
+  },
+  preview: {
+    port: 5014,
   },
 });
